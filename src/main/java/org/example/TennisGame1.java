53bf6256c04378481436567155b72089d644b83e
basel.obied <basel.obied@fawry.com>
2025-12-23 23:15:02 +0200

    refactor: enhance determineDrawScore description: enhancement it by use switch expression to avoid Boilerplate code

--- src/main/java/org/example/TennisGame1.java
+++ src/main/java/org/example/TennisGame1.java
@@ -75,24 +75,12 @@ public class TennisGame1 implements TennisGame {
 
     private String determineDrawScore() {
         final int drawPoints = this.m_score1;
-        String score;
-        switch (drawPoints)
-        {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-
-        }
-        return score;
+        return switch (drawPoints) {
+            case 0 -> "Love-All";
+            case 1 -> "Fifteen-All";
+            case 2 -> "Thirty-All";
+            default -> "Deuce";
+        };
     }
 
     private boolean hasAnyPointsAbove(int points) {
