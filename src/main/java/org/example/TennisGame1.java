bfeb12b3d25c6aa0c6f5950f796edb32acb276e9
basel.obied <basel.obied@fawry.com>
2025-12-23 23:02:27 +0200

    refactor: extract the strategy of each case into methods description: make each strategy to determine the score into methods make the getScore easy for read.

--- src/main/java/org/example/TennisGame1.java
+++ src/main/java/org/example/TennisGame1.java
@@ -25,53 +25,72 @@ public class TennisGame1 implements TennisGame {
         int tempScore=0;
         if (isDraw())
         {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+            final int drawPoints = this.m_score1;
+            score = determineDrawScore(drawPoints);
         }
         else if (hasAnyScoreAbove(4))
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = determineScoreAbove();
         }
         else
         {
-            for (int i=1; i<3; i++)
+            score = determineScoreBelow(score);
+        }
+        return score;
+    }
+
+    private String determineScoreBelow(String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = m_score1;
+            else { score +="-"; tempScore = m_score2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
             }
+        }
+        return score;
+    }
+
+    private String determineScoreAbove() {
+        String score;
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private String determineDrawScore(int drawPoints) {
+        String score;
+        switch (drawPoints)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
         }
         return score;
     }
