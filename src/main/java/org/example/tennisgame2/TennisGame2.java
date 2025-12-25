b88a50e79ec083769a9e0f8e7850a7019b66e432
basel.obied <basel.obied@fawry.com>
2025-12-26 00:08:21 +0200

    refactor: enhance resolveScoreForP1PointsIsHigher description: find pattern for P1 points depend on P1Points > 4

--- src/main/java/org/example/tennisgame2/TennisGame2.java
+++ src/main/java/org/example/tennisgame2/TennisGame2.java
@@ -23,79 +23,77 @@ public class TennisGame2 implements TennisGame
             int drawPoints = P1point;
             return resolveDrawScore(drawPoints);
         } else if(P1point > P2point) {
-            if (P1point > 0 && P2point==0)
-            {
-                if (P1point==1)
-                    P1res = "Fifteen";
-                if (P1point==2)
-                    P1res = "Thirty";
-                if (P1point==3)
-                    P1res = "Forty";
-
-                P2res = "Love";
-                score = P1res + "-" + P2res;
-            }
-
-            if (P1point>P2point && P1point < 4)
-            {
-                if (P1point==2)
-                    P1res="Thirty";
-                if (P1point==3)
-                    P1res="Forty";
-                if (P2point==1)
-                    P2res="Fifteen";
-                if (P2point==2)
-                    P2res="Thirty";
-                score = P1res + "-" + P2res;
-            }
-
-            if (P1point > P2point && P2point > 2)
-            {
-                score = "Advantage player1";
-            }
-            if (P1point>3 && P2point>=0 && (P1point-P2point)>1)
-            {
-                score = "Win for player1";
-            }
+            score = resolveScoreForP1PointsIsHigher();
         } else{
-            if (P2point > 0 && P1point==0)
-            {
-                if (P2point==1)
-                    P2res = "Fifteen";
-                if (P2point==2)
-                    P2res = "Thirty";
-                if (P2point==3)
-                    P2res = "Forty";
-
-                P1res = "Love";
-                score = P1res + "-" + P2res;
-            }
-
-            if (P2point>P1point && P2point < 4)
-            {
-                if (P2point==2)
-                    P2res="Thirty";
-                if (P2point==3)
-                    P2res="Forty";
-                if (P1point==1)
-                    P1res="Fifteen";
-                if (P1point==2)
-                    P1res="Thirty";
-                score = P1res + "-" + P2res;
-            }
-
-            if (P2point > P1point && P1point > 2)
-            {
-                score = "Advantage player2";
-            }
-
-            if (P2point>3 && P1point>=0 && (P2point-P1point)>1)
-            {
-                score = "Win for player2";
-            }
+            score = resolveScoreForP2PointsIsHigher(score);
         }
 
         return score;
+    }
+
+    private String resolveScoreForP2PointsIsHigher(String score) {
+        if (P2point > 0 && P1point==0)
+        {
+            if (P2point==1)
+                P2res = "Fifteen";
+            if (P2point==2)
+                P2res = "Thirty";
+            if (P2point==3)
+                P2res = "Forty";
+
+            P1res = "Love";
+            score = P1res + "-" + P2res;
+        }
+
+        if (P2point>P1point && P2point < 4)
+        {
+            if (P2point==2)
+                P2res="Thirty";
+            if (P2point==3)
+                P2res="Forty";
+            if (P1point==1)
+                P1res="Fifteen";
+            if (P1point==2)
+                P1res="Thirty";
+            score = P1res + "-" + P2res;
+        }
+
+        if (P2point > P1point && P1point > 2)
+        {
+            score = "Advantage player2";
+        }
+
+        if (P2point>3 && P1point>=0 && (P2point-P1point)>1)
+        {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
+    private String resolveScoreForP1PointsIsHigher() {
+
+        if (P1point < 4)
+        {
+            P1res = switch (P1point) {
+                case 1 -> "Fifteen";
+                case 2 -> "Thirty";
+                case 3 -> "Forty";
+                default -> "";
+            };
+            P2res = switch (P2point) {
+                case 0 -> "Love";
+                case 1 -> "Fifteen";
+                case 2 -> "Thirty";
+                default -> "";
+            };
+
+            return P1res + "-" + P2res;
+        } else {
+            if (P1point - P2point > 1) {
+                return "Win for player1";
+            }
+            return "Advantage player1";
+        }
     }
 
     private static String resolveDrawScore(int drawPoints) {
